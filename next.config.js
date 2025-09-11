9cb2533b4711fc4adf789a247c1cc1a6c48fbfb5
tienchung1704 <tien.chungloveu@gmail.com>
2025-09-11 17:53:18 +0700

    next config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
